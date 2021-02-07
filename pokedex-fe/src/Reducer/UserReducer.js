669e012c2ea40c30ba3fbb8937243bedf3aa0f2f
DogeCnx <thiraphat.re0096@gmail.com>
2021-02-07 18:22:12 +0700

    ADD reducer

---
+++ pokedex-fe/src/Reducer/UserReducer.js
@@ -0,0 +1,8 @@
+export default function UserReducer(state = [], action) {
+    switch (action.type){
+        case "LOADED_USER": 
+            return state.concat(action.username)
+        default:
+            return state
+    }
+}
