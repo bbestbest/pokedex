31d054d2831c0ba4b4b09e1531ccdef9427385d9
DogeCnx <thiraphat.re0096@gmail.com>
2021-02-08 19:17:03 +0700

    ADD components fetchBox

--- pokedex-fe/src/components/ModelPadStyle.js
+++ pokedex-fe/src/components/ModelPadStyle.js
@@ -21,6 +21,36 @@ export const ItemPad = styled.div`
   border-radius: 1em;
   flex-direction: column;
 `;
+
+export const ItemPadContent = styled(ItemPad)`
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding-left: 2em;
+`
+
+export const BoxData = styled.div`
+  display: flex;
+  width: 90%;
+  height: 60%;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: row;
+`
+
+export const BoxTextPokemon = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+`
+
+export const BoxImage = styled.div`
+  width: 40%;
+  height: 100%;
+  background-image: url(${(props) => props.image}) ;
+  background-repeat: no-repeat;
+`
+
 export const InputFrom = styled.input`
   display: flex;
   border-radius: 0.5rem;
@@ -29,10 +59,19 @@ export const InputFrom = styled.input`
   width: 15rem;
   height: 2rem;
   margin-bottom: 1rem;
-`;
+`
 
 export const ItemBlock = styled.div`
   padding : 0px;
   border-width : 0px;
   border-style : none;
+`
+
+export const SearchFrom = styled.input`
+  border-radius: 0.5rem;
+  border-style: solid;
+  border-width: 2px;
+  width: 30rem;
+  height: 2rem;
+  margin-bottom: 1rem;
 `
