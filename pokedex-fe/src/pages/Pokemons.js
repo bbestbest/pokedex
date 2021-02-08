f58ffddd086651767ee158dcecd8c989185978c6
DogeCnx <thiraphat.re0096@gmail.com>
2021-02-08 19:16:08 +0700

    Add box for fetch data

--- pokedex-fe/src/pages/Pokemons.js
+++ pokedex-fe/src/pages/Pokemons.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import UserStatus from "../components/UserStatus";
+import Addpokemon from "../components/Addpokemon";
 
 function Pokemons() {
   const { pokedexId } = useParams();
   return (
     <>
-      <UserStatus>Pokedex ID = {pokedexId}</UserStatus>
+      <Addpokemon
+        idParams={pokedexId}
+        image={"https://images.pokemontcg.io/ex8/98.png"}
+        name={"Doge"}
+        type={"DogCoin"}
+        atk={12232}
+        hp={112}
+        res={0}
+      ></Addpokemon>
     </>
   );
 }
