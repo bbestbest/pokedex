e752911ba7fe67279b8ce189861728799f2e0224
DogeCnx <thiraphat.re0096@gmail.com>
2021-02-10 13:47:02 +0700

    EDIT BoxData

--- pokedex-fe/src/components/Addpokemon.js
+++ pokedex-fe/src/components/Addpokemon.js
@@ -1,31 +1,23 @@
 import React from "react";
 import {
-  Container,
-  // ItemPadContent,
-  SearchFrom,
-  // BoxImage,
-  // BoxData,
-  // BoxTextPokemon,
+  BoxData,
+  BoxTextPokemon,
+  BoxImage
+
 } from "./ModelPadStyle";
 
 function Addpokemon(props, { children }) {
   return (
-    <Container>
-      {/* <ItemPadContent> */}
-      <h1>Add pokemon to pokedex #{props.idParams}</h1>
-      <SearchFrom />
-      {/* <BoxData> */}
-      {/* <BoxImage image={props.image} /> */}
-      {/* <BoxTextPokemon> */}
-      <div>Name :{props.name}</div>
-      <div>Type :{props.type}</div>
-      <div>Hp:{props.hp}</div>
-      <div>Atk:{props.atk}</div>
-      <div>Res:{props.res}</div>
-      {/* </BoxTextPokemon> */}
-      {/* </BoxData> */}
-      {/* </ItemPadContent> */}
-    </Container>
+    <BoxData>
+      <BoxImage image={props.image} />
+      <BoxTextPokemon>
+        <div>{props.name}</div>
+        <div>{props.type}</div>
+        <div>{props.atk}</div>
+        <div>{props.hp}</div>
+        <div>{props.res}</div>
+      </BoxTextPokemon>
+    </BoxData>
   );
 }
 
