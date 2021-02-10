5b31d2f989620a21d7a223cb9ac2ae79f355abc6
Your Name <rattaphoom_eikumnerd@hotmail.com>
2021-02-10 23:38:17 +0700

    edit updateById

--- pokedex-be/controllers/UserController.ts
+++ pokedex-be/controllers/UserController.ts
@@ -71,11 +71,9 @@ const updateById = async (req: express.Request, res: express.Response) => {
   const { username, password, pokemon_id } = req.body;
 
   const data = await UserModel.findByIdAndUpdate(id, {
-    $set: {
-      username: username,
-      password: password,
-      pokemon_id: pokemon_id,
-    },
+    username: username,
+    password: password,
+    pokemon_id: pokemon_id,
   });
   return res.status(200).json(data);
 };
