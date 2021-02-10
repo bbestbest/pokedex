a4da282ae5b3e34d6745c8c3de497520f5974407
DogeCnx <thiraphat.re0096@gmail.com>
2021-02-10 13:49:38 +0700

    Delete console.log

--- pokedex-fe/src/Middleware/userSaga.js
+++ pokedex-fe/src/Middleware/userSaga.js
@@ -1,8 +1,5 @@
 import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
-import { GetLogin } from "../services/FetchData";
 
 export function* loadUser(action) {
-  console.log("This is saga dude")
-  console.log(action)
   yield put({ type: "DogeUser", value: action.value });
 }
